c03d3b4cd4d7756368ec45a6878b2af155e9c552
Nehal Garg <115084339+NehalGarg29@users.noreply.github.com>
2023-06-13 18:16:52 +0530

    Add files via upload

---
+++ GREATESR OF THREE.py
@@ -0,0 +1,16 @@
+if __name__=='__main__':
+    a=int(input("ENTER NUM:"))
+    b=int(input("ENTER NUM:"))
+    c=int(input("ENTER NUM:"))
+    
+    if a>b:
+        if a>c:
+            print(a)
+        else:
+            print(c)
+    elif b>c:
+        print(b)
+    else:
+        print(c)
+
+            
